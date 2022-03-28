949622da77367117d092f5b6ecab727958674a50
MartiniGitsi <martin.delrio2000@gmail.com>
2022-03-27 17:04:30 -0700

    texto modificado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require ('express');
 
 const app= express();
 
-app.get('/',(req,res)=> res.send('Hello Mundi con express'))
+app.get('/',(req,res)=> res.send('<h1>Express HTML</h1>'))
 
 
 app.listen (3000);
